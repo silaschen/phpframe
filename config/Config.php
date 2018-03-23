78d4175905155a3b556f5ec8f3c39326c49dea20
root <root@ip-172-31-16-59.ap-northeast-1.compute.internal>
2018-03-23 09:11:12 +0000

    rename

---
+++ config/Config.php
@@ -0,0 +1,37 @@
+<?php 
+namespace config;
+/**
+* config infomation
+*author:silaschen
+*wechat:18811174687
+*/
+class Config
+{
+	const UPLOADPATH = '/var/www';
+	const ALLOW_UPLOAD_FILE = array(
+		'jpg','png','pdf'
+	);
+	public static $webroot="http://sw.com";
+	public static $db=array(
+		'dsn' => "mysql:host=127.0.0.1;dbname=film",
+		'user' => 'root',
+		'password' => 'chen1210'
+	);
+
+	public static $redis=array(
+		'host' => '127.0.0.1',
+		'password' => '',
+		'port'=>6379,
+		'db'=>5
+	);
+
+	public static $mailcfg = array(
+		'host'=>'smtp.qq.com',
+		'mailname'=>'434684326@qq.com',
+		'password'=>'tcgvwzmwmqbdcaii',
+		'from'=>'男宠天气提醒',
+		'port'=>465
+	);
+
+
+}
